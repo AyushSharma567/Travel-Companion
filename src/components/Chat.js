08eecf991d005c58f6f92d0fb95751172177b4d3
Ayush Sharma <93024622+AyushSharma5269@users.noreply.github.com>
2023-05-05 11:35:23 +0530

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,8 +4,7 @@ export class Chat extends Component{
     componentDidMount(){
         (function(d, m){
             var kommunicateSettings = 
-                //{"appId":"16623defd387c6fc6f89b5d720d1dbbdd","popupWidget":true,"automaticChatOpenOnNavigation":true};
-                {"appId":"2a90319b5b41cd904003e027140119552","popupWidget":true,"automaticChatOpenOnNavigation":true};
+                {"appId":process.env.REACT_APP_APP_ID ,"popupWidget":true,"automaticChatOpenOnNavigation":true};
                 var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
             s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
             var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
