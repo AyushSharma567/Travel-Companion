3c3002d0f0f4912d2966956cf09d2e97fd01d4a5
Ayush Sharma <93024622+AyushSharma5269@users.noreply.github.com>
2023-05-05 11:13:51 +0530

    Update firebase.js

--- src/Places-To-Visit/firebase.js
+++ src/Places-To-Visit/firebase.js
@@ -3,21 +3,14 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyDTF8VseayG2JUEckZX_3YoCrbgcG46O0U",
-  // authDomain: "todo-list-d1eee.firebaseapp.com",
-  // databaseURL: "https://todo-list-d1eee-default-rtdb.firebaseio.com",
-  // projectId: "todo-list-d1eee",
-  // storageBucket: "todo-list-d1eee.appspot.com",
-  // messagingSenderId: "1038694320871",
-  // appId: "1:1038694320871:web:b8483480e7552d6e6db042"
-  apiKey: "AIzaSyD6OjImtSJFAjtU_X8DWZve2NAQuuRrBxA",
-  authDomain: "first-last-try.firebaseapp.com",
-  projectId: "first-last-try",
-  storageBucket: "first-last-try.appspot.com",
-  messagingSenderId: "675376603445",
-  appId: "1:675376603445:web:3b25413e60c6a497936c7c",
-  measurementId: "G-7WMG6VSMJM"
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID ,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID
+ };
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
